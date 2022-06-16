439034a79ed8954d691adaca3f856b29e148564d
Alexandr <wormaisan@gmail.com>
2022-06-16 14:36:28 +0600

    сделал шапку и меню бургера для 320px

---
+++ js/common.js
@@ -0,0 +1,56 @@
+/* здесь объявлены глобальные переменные */
+
+let burgerIcon = document.querySelector('.header__burgerIcon');
+let burgerContainer = document.querySelector('.header__burgerContainer');
+
+/*****************************************/
+
+
+window.onresize = function() {
+
+  closeBurgerMenu();
+
+}
+
+/* сработает когда сформируется HTML */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  burgerMenuSetting();
+
+  loadPage();
+
+});
+
+/**********************************************/
+
+
+/* бургер меню */
+
+function burgerMenuSetting() {
+  if (burgerIcon == null) {
+    console.log('не найден класс "header__burgerIcon"')
+  } else {
+    burgerIcon.addEventListener('click', function () {
+      burgerIcon.classList.toggle('active');
+      burgerContainer.classList.toggle('active');
+    })
+  }
+
+  if (burgerContainer == null) {
+    console.log('не найден класс "header__burgerContainer"')
+  } else {
+    burgerContainer.addEventListener('click', function (e) {
+      if (e.target === burgerContainer || e.target.tagName === "A" || e.target.parentElement.tagName === "A") {
+        closeBurgerMenu();
+      }
+    })
+  }
+}
+
+function closeBurgerMenu() {
+  burgerIcon.classList.remove('active');
+  burgerContainer.classList.remove('active');
+}
+
+/*********************/
